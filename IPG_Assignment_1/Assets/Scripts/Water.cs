d5719edfcec527f7847b27cf41edb5e24eb6d77a
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2021-11-15 10:39:08 +0000

    Make jump timeout even smaller, tidy up player start/stop code into new methods in Water.cs and change back to development build

--- IPG_Assignment_1/Assets/Scripts/Water.cs
+++ IPG_Assignment_1/Assets/Scripts/Water.cs
@@ -33,50 +33,52 @@ public class Water : MonoBehaviour
         {
             playerFellInWater = true;
             elapsedTime = 0f;
-
             player = other.gameObject;
-            player.GetComponent<Inventory>().UnequipCurrentItem();
-            player.GetComponent<StarterAssets.ThirdPersonController>().enabled = false;
-            player.GetComponent<PlayerInteraction>().enabled = false;
-            player.GetComponent<CharacterController>().enabled = false;
-            GameObject.Find("PlayerCapsule/Capsule").GetComponent<MeshRenderer>().enabled = false;
-            GameObject.Find("PlayerCapsule/Face").GetComponent<MeshRenderer>().enabled = false;
+            StopPlayer();
             splashSource.PlayOneShot(splashClip);
         }
     }
 
-
     // TODO: consider if there's going to be issues where I have multiple water sections and player falls at a seam triggering two? 
-
     private void Update()
     {
         if (playerFellInWater)
         {
             if (elapsedTime <= fadeInTime)
             {
-
                 info.text = "";
                 message.text = "Oops! I better go dry off...";
                 backgroundImage.alpha += Time.deltaTime / fadeInTime;
             }
-
             else
             {
                 player.transform.position = new Vector3(-12f, 0f, -2f);
-                player.GetComponent<CharacterController>().enabled = true;
-                player.GetComponent<PlayerInteraction>().enabled = true;
-                player.GetComponent<StarterAssets.ThirdPersonController>().enabled = true;
-                GameObject.Find("PlayerCapsule/Capsule").GetComponent<MeshRenderer>().enabled = true;
-                GameObject.Find("PlayerCapsule/Face").GetComponent<MeshRenderer>().enabled = true;
-                player.GetComponent<InputManager>().move = new Vector2(0f, 0f);
+                StartPlayer();
                 playerFellInWater = false;
                 backgroundImage.alpha = 0f;
                 message.text = "";
             }
-
             elapsedTime += Time.deltaTime;
-
         }
     }
 
+    private void StopPlayer()
+    {
+        player.GetComponent<Inventory>().UnequipCurrentItem();
+        player.GetComponent<StarterAssets.ThirdPersonController>().enabled = false;
+        player.GetComponent<PlayerInteraction>().enabled = false;
+        player.GetComponent<CharacterController>().enabled = false;
+        GameObject.Find("PlayerCapsule/Capsule").GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("PlayerCapsule/Face").GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void StartPlayer()
+    {
+        player.GetComponent<CharacterController>().enabled = true;
+        player.GetComponent<PlayerInteraction>().enabled = true;
+        player.GetComponent<StarterAssets.ThirdPersonController>().enabled = true;
+        GameObject.Find("PlayerCapsule/Capsule").GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("PlayerCapsule/Face").GetComponent<MeshRenderer>().enabled = true;
+        player.GetComponent<InputManager>().move = new Vector2(0f, 0f);
+    }
 }
